6c23894ff24cacbd8b6b48d64e9a7e167b2deae9
Tom Poczos <tom@tom-desktop>
2016-03-13 19:38:37 +0000

    typo

--- RestrictedParallelism.hs
+++ RestrictedParallelism.hs
@@ -6,7 +6,7 @@ import Data.List.Split
 -- maps the function to the list parallely in a way so that only the specified number of threads is created
 
 -- Implementation:
--- list is divided into chunks of requierd size
+-- list is divided into chunks of required size
 -- the function is "parMapped" to every sublist and the result retrieved
 -- this ensures that the number of threads created is not larger than the size of sublists
 -- this action of "parMapping" is mapped to every sublist (yeah, gets a bit complicated )
