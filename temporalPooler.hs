814c2e0a250963993ad6ccfd6a0bd7d91e3967fe
Tom Poczos <tom@tom-desktop>
2016-03-21 18:52:33 +0000

    phase 1 learning (partial); compliance modes

--- temporalPooler.hs
+++ temporalPooler.hs
@@ -3,43 +3,51 @@ module TemporalPooler
 ) where
 
 import           Data.List
-import           Data.Maybe
+-- import           Data.Maybe
 import           Flow
 import qualified HtmData    as Htm
 
-phase1 column = if column |> columnPredictedInput
-    then column |> Htm.cells |> map changeStateToActive
-    else column |> Htm.cells |> map changeStateIfCellPredictedInput
+phase1 :: Htm.Region -> Htm.Column -> [Htm.Cell]
+phase1 region column = if column |> columnPredictedInput
+    then column |> Htm.cells |> map changeStateIfCellPredictedInput
+    else column |> Htm.cells |> map  (\cell -> cell {Htm.cellActiveState = True})
 
     where
           changeStateIfCellPredictedInput :: Htm.Cell -> Htm.Cell
-          changeStateIfCellPredictedInput cell = if cell |> cellPredictedInput
-              then changeStateToActive cell
-              else cell
+          changeStateIfCellPredictedInput cell = case region |> Htm.complianceSettings |> Htm.cellLearnStateChange of
+              Htm.Compliant -> if cell |> cellPredictedInput
+                                   then if region |> Htm.learningOn
+                                       then cell {Htm.cellActiveState = True, Htm.cellLearnState = True}
+                                       else cell {Htm.cellActiveState = True}
+                                   else cell
+              Htm.Modified -> if cell |> cellPredictedInput
+                                   then if region |> Htm.learningOn
+                                       then cell {Htm.cellActiveState = True, Htm.cellLearnState = True}
+                                       else cell {Htm.cellActiveState = True, Htm.cellLearnState = False}
+                                   else cell {Htm.cellLearnState = False}
+
+
+
+
 
           cellPredictedInput :: Htm.Cell -> Bool
-          cellPredictedInput cell = isPredictiveCell cell && isInputPredicted cell
-
-          -- Unconditionally changes the cell's state to active
-
-          changeStateToActive :: Htm.Cell -> Htm.Cell
-          changeStateToActive cell = cell {Htm.cellState = Htm.ActiveCell}
+          cellPredictedInput cell = Htm.cellPredictiveState cell && (cell |> isInputPredicted |> fst)
 
           -- Returns True if cell is in predictive state AND has at least one dendrite that is both active AND a sequence segment
 
           columnPredictedInput :: Htm.Column -> Bool
           columnPredictedInput col = case  col |> Htm.cells |> find cellPredictedInput of
-                  Nothing -> False
-                  _       -> True
-
-          -- Returns True if Cell is in predictive state
-
-          isPredictiveCell :: Htm.Cell -> Bool
-          isPredictiveCell cell = Htm.cellState cell == Htm.PredictiveCell
+              Nothing -> False
+              _       -> True
 
           -- returns true if cell has at least one dendrite that is both active AND a sequence segment
 
-          isInputPredicted :: Htm.Cell -> Bool
+          isInputPredicted :: Htm.Cell -> (Bool, Bool)
           isInputPredicted cell = cell
               |> Htm.distalDendrites
-              |> any (\dd -> Htm.distalDendriteState dd == Htm.ActiveDendrite && Htm.sequenceSegment dd)
+              |> find (\dd -> Htm.dendrtiteActiveState dd && Htm.sequenceSegment dd)
+              |> \dendriteThatPredictedInput -> case dendriteThatPredictedInput of
+                    Nothing       -> (False, False)
+                    Just dendrite -> if Htm.dendrtiteLearnState dendrite
+                                         then (True, True)
+                                         else (True, False)
