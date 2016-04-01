dc6ea9d6b642ddf2d36e14a78a6450cc7db84a42
Tom Poczos <tom@tom-desktop>
2016-04-01 21:33:54 +0100

    adding licence and "credits"

--- temporalPooler.hs
+++ temporalPooler.hs
@@ -1,3 +1,27 @@
+{-------------------------------------------------------------------------------
+H2TM: A Haskell HTM/CLA Implementation
+Copyright (C) 2015, Tom Poczos
+
+Developed as part of a Final Year Project at Staffordshire University,
+United Kingdom by Tom Poczos under the supervision of Dr. Mohamed Sedky,
+based on the Hierarchical Temporal Memory paper released by Numenta Inc.:
+
+http://numenta.com/assets/pdf/whitepapers/hierarchical-temporal-memory-cortical-learning-algorithm-0.2.1-en.pdf
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/agpl-3.0
+-------------------------------------------------------------------------------}
+
 module TemporalPooler
 ( temporalPooler
 ) where
